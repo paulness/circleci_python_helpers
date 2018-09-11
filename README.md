ecc64996aa0c8e3e25346d45de9d4a901bdac453
Paul Ness <paulness@artnet.com>
2018-09-11 14:24:34 -0400

    Initial commit with usage instruction in the README

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # set-circleci-env-variables
-Simple Python script to set an environment variable in CircleCI through code.
+Simple Python script to set environment variables in CircleCI through code.
+
+## Usage
+
+
+### Setting one environment variable
+
+``` bash
+export GITHUB_OWNER=<YOUR GITHUB ORG NAME OR USERNAME>
+export GITHUB_REPO=<YOUR GITHUB REPO NAME>
+export CIRCLECI_TOKEN=<YOUR CIRCLECI TOKEN>
+./set_circleci_env_variable.py --key "TEST_KEY" --value "TEST_VALUE"
+```
+
+### Setting multiple environment variables
+
+``` bash
+export GITHUB_OWNER=<YOUR GITHUB ORG NAME OR USERNAME>
+export GITHUB_REPO=<YOUR GITHUB REPO NAME>
+export CIRCLECI_TOKEN=<YOUR CIRCLECI TOKEN>
+./set_circleci_env_variable.py --key "TEST_KEY_01" --value "TEST_VALUE_01"
+./set_circleci_env_variable.py --key "TEST_KEY_02" --value "TEST_VALUE_01"
+```
---
+++ set_circleci_env_variable.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""
+Sets a single circleci environment variable via their v1.1 API interface
+"""
+
+import argparse
+import os
+import requests
+import json
+import sys
+
+
+def set_circleci_env_variable(key, value, github_owner, github_repo, circle_token):
+    """Sets a circleci env variable"""
+
+    url = "https://circleci.com/api/v1.1/project/github/%s/%s/envvar?circle-token=%s" % (github_owner, github_repo, circle_token)
+    payload = { 'name': key, 'value': value }
+    requests.post(url, json=payload)
+
+
+def main():
+    """Sets a circleci env variable from the CLI"""
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--key', required=True)
+    parser.add_argument('--value', required=True)
+    args = parser.parse_args()
+
+    github_owner=os.environ['GITHUB_OWNER']
+    github_repo=os.environ['GITHUB_REPO']
+    circle_token=os.environ['CIRCLECI_TOKEN']
+
+    set_circleci_env_variable(args.key, args.value, github_owner, github_repo, circle_token)
+
+
+if __name__ == "__main__":
+    main()
